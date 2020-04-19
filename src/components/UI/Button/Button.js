fbb3294da47cb57f7d2fb209ac4ad1147fea0223
AdamAnsari <adam.a.ansari@gmail.com>
2020-04-19 17:39:49 +0530

    Implementing the button component

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -5,7 +5,7 @@ const button = (props) => {
 	return(
 		<button
 		className={[classes.Button, classes[props.btnType]].join(' ')}
-		onClick={props.children}>
+		onClick={props.clicked}>
 		{props.children}</button>
 	);
 }
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -107,6 +107,10 @@ class BurgerBuilder extends Component {
 		this.setState({ purchasing: false });
 	} 
 
+	purchaseContinueHandler = () => {
+		alert('You Continue!');
+	} 
+
 	render() {
 		const disabledInfo = {
 			...this.state.ingredients
@@ -118,7 +122,10 @@ class BurgerBuilder extends Component {
 		return(
 			<Auxiliary>
 				<Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-					<OrderSummary ingredients={this.state.ingredients} />
+					<OrderSummary 
+					ingredients={this.state.ingredients} 
+					purchaseCanceled={this.purchaseCancelHandler}
+					purchaseContinued={this.purchaseContinueHandler} />
 				</Modal>
 				<Burger ingredients={this.state.ingredients} />
 				<BuildControls 
