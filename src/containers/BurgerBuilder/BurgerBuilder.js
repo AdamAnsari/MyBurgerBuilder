70a31bacf1cdf534948f97158506fbe1acb94125
AdamAnsari <adam.a.ansari@gmail.com>
2020-04-19 13:47:52 +0530

    Revert "Creating the order summary modal"
    This reverts commit d5efd7f1c237a88ec1aaafbda6e24daf13c066e4.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
-import Modal from '../../components/UI/Modal/Modal';
-import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'; 
 
 const INGREDIENT_PRICES = {
 	salad: 0.5,
@@ -108,9 +106,6 @@ class BurgerBuilder extends Component {
 		}
 		return(
 			<Auxiliary>
-				<Modal>
-					<OrderSummary ingredients={this.state.ingredients} />
-				</Modal>
 				<Burger ingredients={this.state.ingredients} />
 				<BuildControls 
 					ingredientAdded={this.addIngredientHandler} 
