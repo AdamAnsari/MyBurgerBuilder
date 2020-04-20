5e0d12d36343f7e24cf1777b193e15f4d4759d2a
AdamAnsari <adam.a.ansari@gmail.com>
2020-04-20 21:24:07 +0530

    Removing old interceptors

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -33,7 +33,7 @@ class BurgerBuilder extends Component {
 	}
 
 	componentDidMount() {
-		axios.get('/ingredients')
+		axios.get('/ingredients.json')
 			 .then(response => {
 			 	this.setState({ ingredients: response.data })
 			 })
--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -8,19 +8,25 @@ const withErrorHandler = (WrappedComponent, axios) => {
 			error: null,
 		}
 
-		componentDidMount() {
-			axios.interceptors.request.use(req => {
+		componentWillMount() {
+			this.reqInterceptor = axios.interceptors.request.use(req => {
 				this.setState({ 
 					error: null
 				});
 				return req;
 			});	
 
-			axios.interceptors.response.use(res => res, error => {
+			this.resInterceptor = axios.interceptors.response.use(res => res, error => {
 				this.setState({ 
 					error: error
 				});
 			});
+		}
+
+		componentWillUnmount() {
+			console.log('Will Unmount', this.reqInterceptor, this.resInterceptor);
+			axios.interceptors.request.eject(this.reqInterceptor);
+			axios.interceptors.request.eject(this.resInterceptor);
 		}
 
 		errorConfirmedHandler = () => {
